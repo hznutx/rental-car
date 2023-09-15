f971725e843a92c65eae5d25b89b04f60de8a497
hznutx <userhzn@gmail.com>
2023-09-15 14:38:18 +0700

    fix: 🐛 bug fix SearchParams

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -54,7 +54,7 @@ const SearchManufacturer = ({
             afterLeave={() => setQuery("")} // Reset the search query after the transition completes
           >
             <Combobox.Options
-              className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+              className="absolute max-h-96 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
               static
             >
               {filteredManufacturers.length === 0 && query !== "" ? (
